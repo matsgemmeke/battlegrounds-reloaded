50cf704b090279463ca9599e20c06943a0902a10
Mats Gemmeke <matsgemmeke@gmail.com>
2024-11-14 23:08:25 +0100

    Migrated the entity module to JUnit 5

--- core/src/test/java/nl/matsgemmeke/battlegrounds/entity/TrainingModeEntityTest.java
+++ core/src/test/java/nl/matsgemmeke/battlegrounds/entity/TrainingModeEntityTest.java
@@ -1,17 +1,17 @@
 package nl.matsgemmeke.battlegrounds.entity;
 
 import org.bukkit.entity.LivingEntity;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class TrainingModeEntityTest {
 
     private LivingEntity entity;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         this.entity = mock(LivingEntity.class);
     }
