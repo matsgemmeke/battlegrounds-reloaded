6f5227e017683862cd5cac6e59fd5a3f378ed8e0
Mats Gemmeke <mats.gemmeke@alliander.com>
2025-04-29 08:06:23 +0200

    Added implementation for RangeProfileSpec

---
+++ core/src/main/java/nl/matsgemmeke/battlegrounds/configuration/spec/loader/RangeProfileSpecLoader.java
@@ -0,0 +1,75 @@
+package nl.matsgemmeke.battlegrounds.configuration.spec.loader;
+
+import nl.matsgemmeke.battlegrounds.configuration.YamlReader;
+import nl.matsgemmeke.battlegrounds.configuration.spec.FieldSpecResolver;
+import nl.matsgemmeke.battlegrounds.configuration.spec.item.RangeProfileSpec;
+import nl.matsgemmeke.battlegrounds.configuration.validation.RequiredValidator;
+import org.jetbrains.annotations.NotNull;
+
+public class RangeProfileSpecLoader {
+
+    private static final String SHORT_RANGE_DAMAGE_ROUTE = "short-range.damage";
+    private static final String SHORT_RANGE_DISTANCE_ROUTE = "short-range.distance";
+    private static final String MEDIUM_RANGE_DAMAGE_ROUTE = "medium-range.damage";
+    private static final String MEDIUM_RANGE_DISTANCE_ROUTE = "medium-range.distance";
+    private static final String LONG_RANGE_DAMAGE_ROUTE = "long-range.damage";
+    private static final String LONG_RANGE_DISTANCE_ROUTE = "long-range.distance";
+
+    @NotNull
+    private final String baseRoute;
+    @NotNull
+    private final YamlReader yamlReader;
+
+    public RangeProfileSpecLoader(@NotNull YamlReader yamlReader, @NotNull String baseRoute) {
+        this.yamlReader = yamlReader;
+        this.baseRoute = baseRoute;
+    }
+
+    @NotNull
+    public RangeProfileSpec loadSpec() {
+        String shortRangeDamageRoute = this.createRoute(SHORT_RANGE_DAMAGE_ROUTE);
+        String shortRangeDistanceRoute = this.createRoute(SHORT_RANGE_DISTANCE_ROUTE);
+        String mediumRangeDamageRoute = this.createRoute(MEDIUM_RANGE_DAMAGE_ROUTE);
+        String mediumRangeDistanceRoute = this.createRoute(MEDIUM_RANGE_DISTANCE_ROUTE);
+        String longRangeDamageRoute = this.createRoute(LONG_RANGE_DAMAGE_ROUTE);
+        String longRangeDistanceRoute = this.createRoute(LONG_RANGE_DISTANCE_ROUTE);
+
+        Double shortRangeDamage = new FieldSpecResolver<Double>()
+                .route(shortRangeDamageRoute)
+                .value(yamlReader.getOptionalDouble(shortRangeDamageRoute).orElse(null))
+                .validate(new RequiredValidator<>())
+                .resolve();
+        Double shortRangeDistance = new FieldSpecResolver<Double>()
+                .route(shortRangeDistanceRoute)
+                .value(yamlReader.getOptionalDouble(shortRangeDistanceRoute).orElse(null))
+                .validate(new RequiredValidator<>())
+                .resolve();
+        Double mediumRangeDamage = new FieldSpecResolver<Double>()
+                .route(mediumRangeDamageRoute)
+                .value(yamlReader.getOptionalDouble(mediumRangeDamageRoute).orElse(null))
+                .validate(new RequiredValidator<>())
+                .resolve();
+        Double mediumRangeDistance = new FieldSpecResolver<Double>()
+                .route(mediumRangeDistanceRoute)
+                .value(yamlReader.getOptionalDouble(mediumRangeDistanceRoute).orElse(null))
+                .validate(new RequiredValidator<>())
+                .resolve();
+        Double longRangeDamage = new FieldSpecResolver<Double>()
+                .route(longRangeDamageRoute)
+                .value(yamlReader.getOptionalDouble(longRangeDamageRoute).orElse(null))
+                .validate(new RequiredValidator<>())
+                .resolve();
+        Double longRangeDistance = new FieldSpecResolver<Double>()
+                .route(longRangeDistanceRoute)
+                .value(yamlReader.getOptionalDouble(longRangeDistanceRoute).orElse(null))
+                .validate(new RequiredValidator<>())
+                .resolve();
+
+        return new RangeProfileSpec(shortRangeDamage, shortRangeDistance, mediumRangeDamage, mediumRangeDistance, longRangeDamage, longRangeDistance);
+    }
+
+    @NotNull
+    private String createRoute(@NotNull String route) {
+        return baseRoute + "." + route;
+    }
+}
