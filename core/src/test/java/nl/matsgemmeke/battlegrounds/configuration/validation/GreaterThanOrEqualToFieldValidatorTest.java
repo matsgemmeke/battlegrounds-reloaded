1e6e441d970ee8b8f29b46f33e2c903ce16ebf89
Mats Gemmeke <matsgemmeke@gmail.com>
2025-05-05 12:19:13 +0200

    Renamed test

--- core/src/test/java/nl/matsgemmeke/battlegrounds/configuration/validation/GreaterThanOrEqualToFieldValidatorTest.java
+++ core/src/test/java/nl/matsgemmeke/battlegrounds/configuration/validation/GreaterThanOrEqualToFieldValidatorTest.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class GreaterThanOrEqualToValidatorTest {
+public class GreaterThanOrEqualToFieldValidatorTest {
 
     @ParameterizedTest
     @NullSource
