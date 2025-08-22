a08da8abe5f3c0e34679d13e1f76674a0b6bf492
Mats Gemmeke <matsgemmeke@gmail.com>
2025-08-22 18:34:10 +0200

    Implemented game scope usage in PlayerKickEventHandler and PlayerQuitEventHandler

--- core/src/main/java/nl/matsgemmeke/battlegrounds/event/handler/PlayerKickEventHandler.java
+++ core/src/main/java/nl/matsgemmeke/battlegrounds/event/handler/PlayerKickEventHandler.java
@@ -1,33 +1,49 @@
 package nl.matsgemmeke.battlegrounds.event.handler;
 
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 import nl.matsgemmeke.battlegrounds.event.EventHandler;
+import nl.matsgemmeke.battlegrounds.event.EventHandlingException;
+import nl.matsgemmeke.battlegrounds.game.GameContext;
 import nl.matsgemmeke.battlegrounds.game.GameContextProvider;
 import nl.matsgemmeke.battlegrounds.game.GameKey;
+import nl.matsgemmeke.battlegrounds.game.GameScope;
 import nl.matsgemmeke.battlegrounds.game.component.player.PlayerLifecycleHandler;
-import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
 
 public class PlayerKickEventHandler implements EventHandler<PlayerKickEvent> {
 
     @NotNull
-    private final GameContextProvider contextProvider;
+    private final GameContextProvider gameContextProvider;
+    @NotNull
+    private final GameScope gameScope;
+    @NotNull
+    private final Provider<PlayerLifecycleHandler> playerLifecycleHandlerProvider;
 
     @Inject
-    public PlayerKickEventHandler(@NotNull GameContextProvider contextProvider) {
-        this.contextProvider = contextProvider;
+    public PlayerKickEventHandler(@NotNull GameContextProvider gameContextProvider, @NotNull GameScope gameScope, @NotNull Provider<PlayerLifecycleHandler> playerLifecycleHandlerProvider) {
+        this.gameContextProvider = gameContextProvider;
+        this.gameScope = gameScope;
+        this.playerLifecycleHandlerProvider = playerLifecycleHandlerProvider;
     }
 
     public void handle(@NotNull PlayerKickEvent event) {
-        Player player = event.getPlayer();
-        GameKey gameKey = contextProvider.getGameKey(player);
+        UUID playerId = event.getPlayer().getUniqueId();
+        GameKey gameKey = gameContextProvider.getGameKeyByEntityId(playerId).orElse(null);
 
         if (gameKey == null) {
             return;
         }
 
-        PlayerLifecycleHandler playerLifecycleHandler = contextProvider.getComponent(gameKey, PlayerLifecycleHandler.class);
-        playerLifecycleHandler.handlePlayerLeave(player.getUniqueId());
+        GameContext gameContext = gameContextProvider.getGameContext(gameKey)
+                .orElseThrow(() -> new EventHandlingException("Unable to process PlayerKickEvent for game key %s, no corresponding game context was found".formatted(gameKey)));
+
+        gameScope.runInScope(gameContext, () -> {
+            PlayerLifecycleHandler playerLifecycleHandler = playerLifecycleHandlerProvider.get();
+            playerLifecycleHandler.handlePlayerLeave(playerId);
+        });
     }
 }
