7efe06aa4b51ef5480a13fe5593a7540bea50d13
Mats Gemmeke <mats.gemmeke@alliander.com>
2025-11-02 15:11:17 +0100

    Removed old entity hitbox

--- core/src/main/java/nl/matsgemmeke/battlegrounds/configuration/hitbox/HorizontalHitbox.java
+++
@@ -1,7 +0,0 @@
-package nl.matsgemmeke.battlegrounds.configuration.hitbox;
-
-/**
- * A horizontally oriented hitbox.
- */
-public record HorizontalHitbox(double height, double bodyWidth, double headWidth, double legsWidth) {
-}
--- core/src/main/java/nl/matsgemmeke/battlegrounds/configuration/hitbox/VerticalHitbox.java
+++
@@ -1,7 +0,0 @@
-package nl.matsgemmeke.battlegrounds.configuration.hitbox;
-
-/**
- * A vertically oriented hitbox.
- */
-public record VerticalHitbox(double bodyHeight, double headHeight, double legsHeight, double width) {
-}
