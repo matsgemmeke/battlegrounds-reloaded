4b914c3a8da9439cab5a72e794f315e245fe91da
Mats Gemmeke <matsgemmeke@gmail.com>
2025-11-03 08:04:33 +0100

    Added EnemyHitTrigger to TriggerExecutorFactory

--- core/src/test/java/nl/matsgemmeke/battlegrounds/item/trigger/TriggerExecutorFactoryTest.java
+++ core/src/test/java/nl/matsgemmeke/battlegrounds/item/trigger/TriggerExecutorFactoryTest.java
@@ -3,12 +3,16 @@ package nl.matsgemmeke.battlegrounds.item.trigger;
 import com.google.inject.Provider;
 import nl.matsgemmeke.battlegrounds.configuration.item.TriggerSpec;
 import nl.matsgemmeke.battlegrounds.item.trigger.enemy.EnemyProximityTrigger;
+import nl.matsgemmeke.battlegrounds.item.trigger.impl.EnemyHitTrigger;
 import nl.matsgemmeke.battlegrounds.scheduling.Scheduler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -16,26 +20,33 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
-public class TriggerExecutorFactoryTest {
+@ExtendWith(MockitoExtension.class)
+class TriggerExecutorFactoryTest {
 
     private static final double RANGE = 2.5;
     private static final long DELAY = 10L;
     private static final long INTERVAL = 2L;
 
+    @Mock
+    private Provider<EnemyHitTrigger> enemyHitTriggerProvider;
+    @Mock
     private Provider<EnemyProximityTrigger> enemyProximityTriggerProvider;
+    @Mock
     private Scheduler scheduler;
 
+    private TriggerExecutorFactory factory;
+
     @BeforeEach
-    public void setUp() {
-        enemyProximityTriggerProvider = mock();
-        scheduler = mock(Scheduler.class);
+    void setUp() {
+        factory = new TriggerExecutorFactory(enemyHitTriggerProvider, enemyProximityTriggerProvider, scheduler);
     }
 
-    private static Stream<Arguments> invalidTriggerSpecCases() {
+    static Stream<Arguments> invalidTriggerSpecCases() {
         return Stream.of(
+                arguments("ENEMY_HIT", null, INTERVAL, null, RANGE, "delay"),
+                arguments("ENEMY_HIT", DELAY, null, null, RANGE, "interval"),
                 arguments("ENEMY_PROXIMITY", null, INTERVAL, null, RANGE, "delay"),
                 arguments("ENEMY_PROXIMITY", DELAY, null, null, RANGE, "interval"),
                 arguments("ENEMY_PROXIMITY", DELAY, INTERVAL, null, null, "range"),
@@ -49,7 +60,7 @@ public class TriggerExecutorFactoryTest {
 
     @ParameterizedTest
     @MethodSource("invalidTriggerSpecCases")
-    public void createThrowsTriggerCreationExceptionWhenRequiredValuesInSpecAreNull(String type, Long delay, Long interval, List<Long> offsetDelays, Double range, String requiredValue) {
+    void createThrowsTriggerCreationExceptionWhenRequiredValuesInSpecAreNull(String type, Long delay, Long interval, List<Long> offsetDelays, Double range, String requiredValue) {
         TriggerSpec spec = new TriggerSpec();
         spec.type = type;
         spec.delay = delay;
@@ -59,15 +70,29 @@ public class TriggerExecutorFactoryTest {
 
         String expectedErrorMessage = "Cannot create trigger %s because of invalid spec: Required '%s' value is missing".formatted(type, requiredValue);
 
-        TriggerExecutorFactory factory = new TriggerExecutorFactory(enemyProximityTriggerProvider, scheduler);
-
         assertThatThrownBy(() -> factory.create(spec))
                 .isInstanceOf(TriggerCreationException.class)
                 .hasMessage(expectedErrorMessage);
     }
 
     @Test
-    public void createReturnsEnemyProximityTriggerInstanceWhenTripperTypeEqualsEnemyProximity() {
+    void createReturnsTriggerExecutorWithEnemyHitTriggerWhenTriggerTypeEqualsEnemyHit() {
+        TriggerSpec spec = new TriggerSpec();
+        spec.type = "ENEMY_HIT";
+        spec.delay = DELAY;
+        spec.interval = INTERVAL;
+        spec.repeating = true;
+
+        EnemyHitTrigger trigger = mock(EnemyHitTrigger.class);
+        when(enemyHitTriggerProvider.get()).thenReturn(trigger);
+
+        TriggerExecutor triggerExecutor = factory.create(spec);
+
+        assertThat(triggerExecutor.isRepeating()).isTrue();
+    }
+
+    @Test
+    void createReturnsTriggerExecutorWithEnemyProximityTriggerInstanceWhenTripperTypeEqualsEnemyProximity() {
         TriggerSpec spec = new TriggerSpec();
         spec.type = "ENEMY_PROXIMITY";
         spec.delay = DELAY;
@@ -78,58 +103,55 @@ public class TriggerExecutorFactoryTest {
         EnemyProximityTrigger trigger = mock(EnemyProximityTrigger.class);
         when(enemyProximityTriggerProvider.get()).thenReturn(trigger);
 
-        TriggerExecutorFactory factory = new TriggerExecutorFactory(enemyProximityTriggerProvider, scheduler);
         TriggerExecutor triggerExecutor = factory.create(spec);
 
         assertThat(triggerExecutor.isRepeating()).isTrue();
+
+        verify(trigger).setCheckingRange(RANGE);
     }
 
     @Test
-    public void createReturnsFloorHitTriggerInstanceWhenTriggerTypeEqualsFloorHit() {
+    void createReturnsTriggerExecutorWithFloorHitTriggerInstanceWhenTriggerTypeEqualsFloorHit() {
         TriggerSpec spec = new TriggerSpec();
         spec.type = "FLOOR_HIT";
         spec.delay = DELAY;
         spec.interval = INTERVAL;
         spec.range = RANGE;
 
-        TriggerExecutorFactory factory = new TriggerExecutorFactory(enemyProximityTriggerProvider, scheduler);
         TriggerExecutor triggerExecutor = factory.create(spec);
 
         assertThat(triggerExecutor.isRepeating()).isFalse();
     }
 
     @Test
-    public void createReturnsImpactTriggerInstanceWhenTriggerTypeEqualsImpact() {
+    void createReturnsTriggerExecutorWithImpactTriggerInstanceWhenTriggerTypeEqualsImpact() {
         TriggerSpec spec = new TriggerSpec();
         spec.type = "IMPACT";
         spec.delay = DELAY;
         spec.interval = INTERVAL;
 
-        TriggerExecutorFactory factory = new TriggerExecutorFactory(enemyProximityTriggerProvider, scheduler);
         TriggerExecutor triggerExecutor = factory.create(spec);
 
         assertThat(triggerExecutor.isRepeating()).isFalse();
     }
 
     @Test
-    public void createReturnsScheduledTriggerInstanceWithSingleRunScheduleWhenTriggerTypeEqualsDelayed() {
+    void createReturnsTriggerExecutorWithScheduledTriggerInstanceWithSingleRunScheduleWhenTriggerTypeEqualsDelayed() {
         TriggerSpec spec = new TriggerSpec();
         spec.type = "SCHEDULED";
         spec.offsetDelays = List.of(DELAY);
 
-        TriggerExecutorFactory factory = new TriggerExecutorFactory(enemyProximityTriggerProvider, scheduler);
         TriggerExecutor triggerExecutor = factory.create(spec);
 
         assertThat(triggerExecutor.isRepeating()).isFalse();
     }
 
     @Test
-    public void createReturnsScheduledTriggerInstanceWithSequenceScheduleWhenTriggerTypeEqualsDelayed() {
+    void createReturnsTriggerExecutorWithScheduledTriggerInstanceWithSequenceScheduleWhenTriggerTypeEqualsDelayed() {
         TriggerSpec spec = new TriggerSpec();
         spec.type = "SCHEDULED";
         spec.offsetDelays = List.of(10L, 20L);
 
-        TriggerExecutorFactory factory = new TriggerExecutorFactory(enemyProximityTriggerProvider, scheduler);
         TriggerExecutor triggerExecutor = factory.create(spec);
 
         assertThat(triggerExecutor.isRepeating()).isFalse();
