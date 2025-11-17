d989d7d894789c85b2d07bb8f7ae0b2b7953eb2e
Mats Gemmeke <mats.gemmeke@alliander.com>
2025-11-17 07:24:14 +0100

    Add hitbox tests

--- core/src/test/java/nl/matsgemmeke/battlegrounds/entity/hitbox/provider/PlayerHitboxProviderTest.java
+++ core/src/test/java/nl/matsgemmeke/battlegrounds/entity/hitbox/provider/PlayerHitboxProviderTest.java
@@ -1,16 +1,23 @@
 package nl.matsgemmeke.battlegrounds.entity.hitbox.provider;
 
 import nl.matsgemmeke.battlegrounds.entity.hitbox.Hitbox;
+import nl.matsgemmeke.battlegrounds.entity.hitbox.HitboxComponent;
 import nl.matsgemmeke.battlegrounds.entity.hitbox.RelativeHitbox;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.type.Bed;
+import org.bukkit.block.data.type.Sign;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Pose;
 import org.bukkit.entity.Zombie;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Collections;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -20,9 +27,9 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class PlayerHitboxProviderTest {
 
-    private static final RelativeHitbox STANDING_HITBOX = new RelativeHitbox(Collections.emptySet());
-    private static final RelativeHitbox SNEAKING_HITBOX = new RelativeHitbox(Collections.emptySet());
-    private static final RelativeHitbox SLEEPING_HITBOX = new RelativeHitbox(Collections.emptySet());
+    private static final RelativeHitbox STANDING_HITBOX = new RelativeHitbox(Set.of(new HitboxComponent(null, 1, 1, 1, 1, 1, 1)));
+    private static final RelativeHitbox SNEAKING_HITBOX = new RelativeHitbox(Set.of(new HitboxComponent(null, 2, 2, 2, 2, 2, 2)));
+    private static final RelativeHitbox SLEEPING_HITBOX = new RelativeHitbox(Set.of(new HitboxComponent(null, 3, 3, 3, 3, 3, 3)));
 
     private final PlayerHitboxProvider hitboxProvider = new PlayerHitboxProvider(STANDING_HITBOX, SNEAKING_HITBOX, SLEEPING_HITBOX);
 
@@ -43,17 +50,46 @@ class PlayerHitboxProviderTest {
 
         Hitbox hitbox = hitboxProvider.provideHitbox(player);
 
-        assertThat(hitbox.getComponents()).isSameAs(SNEAKING_HITBOX.components());
+        assertThat(hitbox.getComponents()).containsAll(SNEAKING_HITBOX.components());
     }
 
     @Test
-    void provideHitboxReturnsSneakingHitboxWhenPlayerIsSleeping() {
+    void provideHitboxReturnsSleepingHitboxWhenPlayerIsSleeping() {
+        Location location = new Location(null, 1, 1, 1);
+
+        Bed bed = mock(Bed.class);
+        when(bed.getFacing()).thenReturn(BlockFace.EAST);
+
+        World world = mock(World.class, Mockito.RETURNS_DEEP_STUBS);
+        when(world.getBlockAt(location).getBlockData()).thenReturn(bed);
+
+        location.setWorld(world);
+
         Player player = mock(Player.class);
+        when(player.getLocation()).thenReturn(location);
         when(player.getPose()).thenReturn(Pose.SLEEPING);
 
         Hitbox hitbox = hitboxProvider.provideHitbox(player);
 
-        assertThat(hitbox.getComponents()).isSameAs(SLEEPING_HITBOX.components());
+        assertThat(hitbox.getComponents()).containsAll(SLEEPING_HITBOX.components());
+    }
+
+    @Test
+    void provideHitboxReturnsStandingHitboxWhenPlayerIsSleepingButTheirLocationIsNoBed() {
+        Location location = new Location(null, 1, 1, 1);
+
+        World world = mock(World.class, Mockito.RETURNS_DEEP_STUBS);
+        when(world.getBlockAt(location).getBlockData()).thenReturn(mock(Sign.class));
+
+        location.setWorld(world);
+
+        Player player = mock(Player.class);
+        when(player.getLocation()).thenReturn(location);
+        when(player.getPose()).thenReturn(Pose.SLEEPING);
+
+        Hitbox hitbox = hitboxProvider.provideHitbox(player);
+
+        assertThat(hitbox.getComponents()).containsAll(STANDING_HITBOX.components());
     }
 
     @Test
@@ -63,6 +99,6 @@ class PlayerHitboxProviderTest {
 
         Hitbox hitbox = hitboxProvider.provideHitbox(player);
 
-        assertThat(hitbox.getComponents()).isSameAs(STANDING_HITBOX.components());
+        assertThat(hitbox.getComponents()).containsAll(STANDING_HITBOX.components());
     }
 }
