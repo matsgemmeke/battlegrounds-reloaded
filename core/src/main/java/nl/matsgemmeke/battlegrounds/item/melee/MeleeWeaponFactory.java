875dc9d8b768953131add09501ca24bd76017004
Mats Gemmeke <matsgemmeke@gmail.com>
2025-11-29 18:51:52 +0100

    Included display item template in MeleeWeaponFactory

--- core/src/main/java/nl/matsgemmeke/battlegrounds/item/melee/MeleeWeaponFactory.java
+++ core/src/main/java/nl/matsgemmeke/battlegrounds/item/melee/MeleeWeaponFactory.java
@@ -1,17 +1,33 @@
 package nl.matsgemmeke.battlegrounds.item.melee;
 
 import com.google.inject.Inject;
+import nl.matsgemmeke.battlegrounds.configuration.item.ItemSpec;
 import nl.matsgemmeke.battlegrounds.configuration.item.melee.MeleeWeaponSpec;
 import nl.matsgemmeke.battlegrounds.game.component.item.MeleeWeaponRegistry;
+import nl.matsgemmeke.battlegrounds.item.ItemTemplate;
+import nl.matsgemmeke.battlegrounds.item.PersistentDataEntry;
 import nl.matsgemmeke.battlegrounds.item.controls.ItemControls;
+import nl.matsgemmeke.battlegrounds.text.TextTemplate;
+import nl.matsgemmeke.battlegrounds.util.NamespacedKeyCreator;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.UUID;
 
 public class MeleeWeaponFactory {
 
+    private static final String ACTION_EXECUTOR_ID_KEY = "action-executor-id";
+    private static final String ACTION_EXECUTOR_ID_VALUE = "melee-weapon";
+    private static final String TEMPLATE_ID_KEY = "template-id";
+
     private final MeleeWeaponRegistry meleeWeaponRegistry;
+    private final NamespacedKeyCreator namespacedKeyCreator;
 
     @Inject
-    public MeleeWeaponFactory(MeleeWeaponRegistry meleeWeaponRegistry) {
+    public MeleeWeaponFactory(MeleeWeaponRegistry meleeWeaponRegistry, NamespacedKeyCreator namespacedKeyCreator) {
         this.meleeWeaponRegistry = meleeWeaponRegistry;
+        this.namespacedKeyCreator = namespacedKeyCreator;
     }
 
     public MeleeWeapon create(MeleeWeaponSpec spec) {
@@ -35,7 +51,29 @@ public class MeleeWeaponFactory {
         DefaultMeleeWeapon meleeWeapon = new DefaultMeleeWeapon(new ItemControls<>());
         meleeWeapon.setName(spec.name);
         meleeWeapon.setDescription(spec.description);
+        meleeWeapon.setAttackDamage(spec.damage.damageAmount);
+
+        ItemTemplate displayItemTemplate = this.createDisplayItemTemplate(spec.items.displayItem);
+        meleeWeapon.setDisplayItemTemplate(displayItemTemplate);
+        meleeWeapon.update();
 
         return meleeWeapon;
+    }
+
+    private ItemTemplate createDisplayItemTemplate(ItemSpec spec) {
+        NamespacedKey templateKey = namespacedKeyCreator.create(TEMPLATE_ID_KEY);
+        UUID templateId = UUID.randomUUID();
+        Material material = Material.valueOf(spec.material);
+        String displayName = spec.displayName;
+        int damage = spec.damage;
+
+        NamespacedKey actionExecutorIdKey = namespacedKeyCreator.create(ACTION_EXECUTOR_ID_KEY);
+        PersistentDataEntry<String, String> actionExecutorIdDataEntry = new PersistentDataEntry<>(actionExecutorIdKey, PersistentDataType.STRING, ACTION_EXECUTOR_ID_VALUE);
+
+        ItemTemplate itemTemplate = new ItemTemplate(templateKey, templateId, material);
+        itemTemplate.addPersistentDataEntry(actionExecutorIdDataEntry);
+        itemTemplate.setDamage(damage);
+        itemTemplate.setDisplayNameTemplate(new TextTemplate(displayName));
+        return itemTemplate;
     }
 }
--- core/src/test/java/nl/matsgemmeke/battlegrounds/item/melee/MeleeWeaponFactoryTest.java
+++ core/src/test/java/nl/matsgemmeke/battlegrounds/item/melee/MeleeWeaponFactoryTest.java
@@ -4,25 +4,56 @@ import nl.matsgemmeke.battlegrounds.configuration.item.melee.MeleeWeaponSpec;
 import nl.matsgemmeke.battlegrounds.configuration.spec.SpecDeserializer;
 import nl.matsgemmeke.battlegrounds.entity.GamePlayer;
 import nl.matsgemmeke.battlegrounds.game.component.item.MeleeWeaponRegistry;
+import nl.matsgemmeke.battlegrounds.util.NamespacedKeyCreator;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemFactory;
+import org.bukkit.plugin.Plugin;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.File;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mockStatic;
 
 @ExtendWith(MockitoExtension.class)
 class MeleeWeaponFactoryTest {
 
     @Mock
     private MeleeWeaponRegistry meleeWeaponRegistry;
+    @Mock
+    private NamespacedKeyCreator namespacedKeyCreator;
     @InjectMocks
     private MeleeWeaponFactory meleeWeaponFactory;
+
+    private MockedStatic<Bukkit> bukkit;
+
+    @BeforeEach
+    void setUp() {
+        Plugin plugin = mock(Plugin.class);
+        when(plugin.getName()).thenReturn("Battlegrounds");
+
+        ItemFactory itemFactory = mock(ItemFactory.class);
+        NamespacedKey key = new NamespacedKey(plugin, "template-id");
+
+        when(namespacedKeyCreator.create("template-id")).thenReturn(key);
+
+        bukkit = mockStatic(Bukkit.class);
+        bukkit.when(Bukkit::getItemFactory).thenReturn(itemFactory);
+    }
+
+    @AfterEach
+    void tearDown() {
+        bukkit.close();
+    }
 
     @Test
     void createReturnsMeleeWeaponWithoutAssignedHolder() {
@@ -33,6 +64,7 @@ class MeleeWeaponFactoryTest {
         assertThat(meleeWeapon.getName()).isEqualTo("Combat Knife");
         assertThat(meleeWeapon.getDescription()).isEqualTo("Standard issue military knife. Fast, quiet and deadly.");
         assertThat(meleeWeapon.getHolder()).isEmpty();
+        assertThat(meleeWeapon.getAttackDamage()).isEqualTo(75.0);
 
         verify(meleeWeaponRegistry).register(meleeWeapon);
     }
@@ -47,6 +79,7 @@ class MeleeWeaponFactoryTest {
         assertThat(meleeWeapon.getName()).isEqualTo("Combat Knife");
         assertThat(meleeWeapon.getDescription()).isEqualTo("Standard issue military knife. Fast, quiet and deadly.");
         assertThat(meleeWeapon.getHolder()).hasValue(gamePlayer);
+        assertThat(meleeWeapon.getAttackDamage()).isEqualTo(75.0);
 
         verify(meleeWeaponRegistry).register(meleeWeapon, gamePlayer);
     }
