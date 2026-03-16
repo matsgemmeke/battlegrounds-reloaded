0ee87dc19cd54e67f99e09af6d99cabb1d9f0c53
Mats Gemmeke <matsgemmeke@gmail.com>
2026-03-16 08:56:28 +0100

    Remove enum options in violation message

--- core/src/main/java/nl/matsgemmeke/battlegrounds/validation/common/validator/EnumValueValidator.java
+++ core/src/main/java/nl/matsgemmeke/battlegrounds/validation/common/validator/EnumValueValidator.java
@@ -8,8 +8,7 @@ import java.util.Arrays;
 
 public class EnumValueValidator implements ConstraintValidator<EnumValue, Object> {
 
-    private static final String MESSAGE_TEMPLATE = "Invalid value '%s' for enum %s. Allowed values: %s";
-    private static final int LARGE_ALLOWED_VALUES_SIZE = 10;
+    private static final String MESSAGE_TEMPLATE = "invalid value '%s' for enum %s";
 
     private Class<? extends Enum<?>> type;
 
@@ -28,22 +27,10 @@ public class EnumValueValidator implements ConstraintValidator<EnumValue, Object
             return true;
         }
 
-        String allowedValues = this.formatAllowedValues(type);
-        String message = MESSAGE_TEMPLATE.formatted(value, type.getSimpleName(), allowedValues);
+        String message = MESSAGE_TEMPLATE.formatted(value, type.getSimpleName());
 
         constraintValidatorContext.disableDefaultConstraintViolation();
         constraintValidatorContext.buildConstraintViolationWithTemplate(message).addConstraintViolation();
         return false;
-    }
-
-    private String formatAllowedValues(Class<? extends Enum<?>> type) {
-        Enum<?>[] constants = type.getEnumConstants();
-        int size = constants.length;
-
-        if (size >= LARGE_ALLOWED_VALUES_SIZE) {
-            return Arrays.stream(constants).limit(LARGE_ALLOWED_VALUES_SIZE).toList() + " ... (" + (size - LARGE_ALLOWED_VALUES_SIZE) + " more)";
-        } else {
-            return Arrays.stream(constants).map(Enum::name).toList().toString();
-        }
     }
 }
--- core/src/test/java/nl/matsgemmeke/battlegrounds/validation/common/validator/EnumValueValidatorTest.java
+++ core/src/test/java/nl/matsgemmeke/battlegrounds/validation/common/validator/EnumValueValidatorTest.java
@@ -32,7 +32,7 @@ class EnumValueValidatorTest {
     @DisplayName("isValid returns true when given value is a valid enum type")
     void isValid_validEnumValue(String value) {
         TestValidationObject object = new TestValidationObject();
-        object.enumValueLargeEnum = value;
+        object.enumValue = value;
 
         Set<ConstraintViolation<TestValidationObject>> violations = validator.validate(object);
 
@@ -40,28 +40,15 @@ class EnumValueValidatorTest {
     }
 
     @Test
-    @DisplayName("isValid returns false when given value is not a valid type of a small enum")
-    void isValid_invalidValueSmallEnum() {
-        TestValidationObject object = new TestValidationObject();
-        object.enumValueSmallEnum = "fail";
-
-        Set<ConstraintViolation<TestValidationObject>> violations = validator.validate(object);
-
-        assertThat(violations).satisfiesExactly(violation -> {
-            assertThat(violation.getMessage()).isEqualTo("Invalid value 'fail' for enum HitboxComponentType. Allowed values: [HEAD, TORSO, LIMBS]");
-        });
-    }
-
-    @Test
-    @DisplayName("isValid returns false when given value is not a valid type of a large enum")
+    @DisplayName("isValid returns false when given value is not a valid type of enum")
     void isValid_invalidValueLargeEnum() {
         TestValidationObject object = new TestValidationObject();
-        object.enumValueLargeEnum = "fail";
+        object.enumValue = "fail";
 
         Set<ConstraintViolation<TestValidationObject>> violations = validator.validate(object);
 
         assertThat(violations).satisfiesExactly(violation -> {
-            assertThat(violation.getMessage()).isEqualTo("Invalid value 'fail' for enum Particle. Allowed values: [EXPLOSION_NORMAL, EXPLOSION_LARGE, EXPLOSION_HUGE, FIREWORKS_SPARK, WATER_BUBBLE, WATER_SPLASH, WATER_WAKE, SUSPENDED, SUSPENDED_DEPTH, CRIT] ... (92 more)");
+            assertThat(violation.getMessage()).isEqualTo("invalid value 'fail' for enum Particle");
         });
     }
 }
