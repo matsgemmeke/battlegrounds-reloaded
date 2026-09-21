11e69bf0625de57981df1ff074b920563a271363
Mats Gemmeke <matsgemmeke@gmail.com>
2026-09-21 08:22:44 +0200

    Refactor tests for new GameContext implementations

--- core/src/test/java/nl/matsgemmeke/battlegrounds/game/component/entity/MobRegistryProviderTest.java
+++ core/src/test/java/nl/matsgemmeke/battlegrounds/game/component/entity/MobRegistryProviderTest.java
@@ -2,17 +2,20 @@ package nl.matsgemmeke.battlegrounds.game.component.entity;
 
 import com.google.inject.Provider;
 import com.google.inject.TypeLiteral;
-import nl.matsgemmeke.battlegrounds.game.GameContext;
+import nl.matsgemmeke.battlegrounds.freeplay.FreeplayGameContext;
 import nl.matsgemmeke.battlegrounds.game.GameContextType;
-import nl.matsgemmeke.battlegrounds.game.GameKey;
 import nl.matsgemmeke.battlegrounds.game.GameScope;
 import nl.matsgemmeke.battlegrounds.game.component.ComponentProvisionException;
 import nl.matsgemmeke.battlegrounds.game.freeplay.component.entity.FreeplayMobRegistry;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -24,52 +27,54 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class MobRegistryProviderTest {
 
-    private static final TypeLiteral<MobRegistry> TYPE_LITERAL = TypeLiteral.get(MobRegistry.class);
+    private static final FreeplayGameContext GAME_CONTEXT = new FreeplayGameContext();
 
     @Mock
     private GameScope gameScope;
+    @Spy
+    private Map<GameContextType, Provider<MobRegistry>> implementations = new HashMap<>();
+    @Spy
+    private TypeLiteral<MobRegistry> TYPE_LITERAL = TypeLiteral.get(MobRegistry.class);
+    @InjectMocks
+    private MobRegistryProvider mobRegistryProvider;
 
     @Test
-    void getThrowsComponentProvisionExceptionWhenGameScopeHasNoEnteredGameContext() {
-        Map<GameContextType, Provider<MobRegistry>> implementations = Map.of();
-
+    @DisplayName("get throws ComponentProvisionException when game scope has no entered game context")
+    void get_noEnteredGameContext() {
         when(gameScope.getCurrentGameContext()).thenReturn(Optional.empty());
 
-        MobRegistryProvider provider = new MobRegistryProvider(gameScope, implementations, TYPE_LITERAL);
-
-        assertThatThrownBy(provider::get)
+        assertThatThrownBy(mobRegistryProvider::get)
                 .isInstanceOf(ComponentProvisionException.class)
                 .hasMessage("Cannot provide instance of MobRegistry: the game scope is empty");
     }
 
     @Test
-    void getThrowsComponentProvisionExceptionWhenImplementationContainsNoProviderForGameContextType() {
-        GameContext gameContext = new GameContext(GameKey.ofFreeplay(), GameContextType.ARENA_MODE);
-        Map<GameContextType, Provider<MobRegistry>> implementations = Map.of(GameContextType.FREEPLAY_MODE, mock());
+    @DisplayName("get throws ComponentProvisionException when implementations contains no provider for current game context type")
+    void get_noCompatibleProvider() {
+        implementations.put(GameContextType.ARENA_MODE, mock());
 
-        when(gameScope.getCurrentGameContext()).thenReturn(Optional.of(gameContext));
+        when(gameScope.getCurrentGameContext()).thenReturn(Optional.of(GAME_CONTEXT));
 
         MobRegistryProvider provider = new MobRegistryProvider(gameScope, implementations, TYPE_LITERAL);
 
         assertThatThrownBy(provider::get)
                 .isInstanceOf(ComponentProvisionException.class)
-                .hasMessage("Cannot provide instance of MobRegistry: no implementation bound for ARENA_MODE");
+                .hasMessage("Cannot provide instance of MobRegistry: no implementation bound for FREEPLAY_MODE");
     }
 
     @Test
-    void getReturnsInstanceBoundToTypeOfActiveGameContext() {
-        GameContext gameContext = new GameContext(GameKey.ofFreeplay(), GameContextType.FREEPLAY_MODE);
+    @DisplayName("get returns provider bound to active game context type")
+    void get_successful() {
         FreeplayMobRegistry mobRegistry = mock(FreeplayMobRegistry.class);
 
-        Provider<MobRegistry> mobRegistryProvider = mock();
-        when(mobRegistryProvider.get()).thenReturn(mobRegistry);
+        Provider<MobRegistry> freeplayMobRegistryProvider = mock();
+        when(freeplayMobRegistryProvider.get()).thenReturn(mobRegistry);
 
-        Map<GameContextType, Provider<MobRegistry>> implementations = Map.of(GameContextType.FREEPLAY_MODE, mobRegistryProvider);
+        implementations.put(GameContextType.FREEPLAY_MODE, freeplayMobRegistryProvider);
 
-        when(gameScope.getCurrentGameContext()).thenReturn(Optional.of(gameContext));
+        when(gameScope.getCurrentGameContext()).thenReturn(Optional.of(GAME_CONTEXT));
 
-        MobRegistryProvider provider = new MobRegistryProvider(gameScope, implementations, TYPE_LITERAL);
-        MobRegistry result = provider.get();
+        MobRegistry result = mobRegistryProvider.get();
 
         assertThat(result).isEqualTo(mobRegistry);
     }
